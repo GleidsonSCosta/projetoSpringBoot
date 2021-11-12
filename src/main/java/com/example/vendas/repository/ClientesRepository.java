d3f5ca52677028dbe3573186ba98398eea4c98ff
Gleidson Costa <gleidsoncosta92@hotmail.com>
2021-11-11 21:10:20 -0300

    Perfis - Injecao de Dep

---
+++ src/main/java/com/example/vendas/repository/ClientesRepository.java
@@ -0,0 +1,14 @@
+package com.example.vendas.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.vendas.model.Cliente;
+
+@Repository
+public class ClientesRepository {
+
+	public void persistir(Cliente cliente) {
+		// acessa a base de dados e salva o cliente	
+	}
+	
+}
