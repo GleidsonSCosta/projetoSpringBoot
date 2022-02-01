cfb1af951ccb26d5b89cb2c254106c4cd4c62c3f
Gleidson Costa <gleidsoncosta92@hotmail.com>
2022-02-01 13:19:41 -0300

    gerendo token JWT

---
+++ src/main/java/com/example/vendas/JwtService.java
@@ -0,0 +1,37 @@
+package com.example.vendas;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.example.vendas.model.Usuarios;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service	
+public class JwtService {
+	
+	@Value("${security.jwt.expiracao}")
+	private String expiracao;
+	
+	@Value("${security.jwt.chave-assinatura}")
+	private String chaveAssinatura;
+	
+	public String gerarToken(Usuarios usuario) {
+		long expString = Long.valueOf(expiracao);
+		LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+		Date data = Date.from(dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant());
+		return Jwts
+				.builder()
+				.setSubject(usuario.getLogin())
+				.setExpiration(data)
+				.signWith(SignatureAlgorithm.HS512, chaveAssinatura)
+				.compact();
+		
+				
+	}
+}
